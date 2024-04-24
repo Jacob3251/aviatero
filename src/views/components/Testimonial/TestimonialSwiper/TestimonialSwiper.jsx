266cfa4eac0eeb164556e066bc9d50f2dcacc758
Md. Nayeem Hasan Adil <jacobfrye3251@gmail.com>
2024-04-24 18:36:38 +0600

    testimonial done

--- src/views/components/Testimonial/TestimonialSwiper/TestimonialSwiper.jsx
+++ src/views/components/Testimonial/TestimonialSwiper/TestimonialSwiper.jsx
@@ -60,20 +60,215 @@ function TestimonialSwiper() {
                   />
                 </div>
                 <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
-                  <div className="text-[18px] sm:text-[24px] xl:text-[32px] font-noto text-primary font-medium mb-2 xl:mb-[20px]">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
                     Nazia Islam
                   </div>
-                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 xl:mb-[20px]">
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
                     <GoLocation /> <span>Sylhet, Bangladesh </span>
                   </div>
-                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 xl:mb-[20px]">
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
                     <FaStar />
                     <FaStar />
                     <FaStar />
                     <FaStar />
                     <FaStar />
                   </div>
-                  <div className="flex justify-start mb-2 xl:mb-[45px]">
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
+                    <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
+                      Aviate Abroad helped turn my dream of traveling abroad
+                      into a reality! Their knowledgeable team provided me with
+                      all the information I needed to plan my ...
+                    </span>{" "}
+                  </div>
+                  <div className="text-primary font-monrope text-[14px] sm:text-[18px] xl:text-[16px] font-bold flex items-center">
+                    <span>Full Story</span>{" "}
+                    <MdOutlineKeyboardArrowRight className="text-[16px] sm:text-[28px] xl:text-[24px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-[75%]  max-h-full sm:max-h-full  h-full mx-auto   overflow-hidden flex justify-center items-center ">
+              <div className="flex flex-col md:flex-row testimonial-item w-full space-y-5 xl:space-y-0 md:space-x-[40px]  justify-center items-center px-[80px] ">
+                <div className="w-full h-[240px] sm:h-[370px] xl:w-[500px] xl:h-full ">
+                  <img
+                    src={img1}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
+                    Nazia Islam
+                  </div>
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
+                    <GoLocation /> <span>Sylhet, Bangladesh </span>
+                  </div>
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
+                    <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
+                      Aviate Abroad helped turn my dream of traveling abroad
+                      into a reality! Their knowledgeable team provided me with
+                      all the information I needed to plan my ...
+                    </span>{" "}
+                  </div>
+                  <div className="text-primary font-monrope text-[14px] sm:text-[18px] xl:text-[16px] font-bold flex items-center">
+                    <span>Full Story</span>{" "}
+                    <MdOutlineKeyboardArrowRight className="text-[16px] sm:text-[28px] xl:text-[24px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-[75%]  max-h-full sm:max-h-full  h-full mx-auto   overflow-hidden flex justify-center items-center ">
+              <div className="flex flex-col md:flex-row testimonial-item w-full space-y-5 xl:space-y-0 md:space-x-[40px]  justify-center items-center px-[80px] ">
+                <div className="w-full h-[240px] sm:h-[370px] xl:w-[500px] xl:h-full ">
+                  <img
+                    src={img1}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
+                    Nazia Islam
+                  </div>
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
+                    <GoLocation /> <span>Sylhet, Bangladesh </span>
+                  </div>
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
+                    <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
+                      Aviate Abroad helped turn my dream of traveling abroad
+                      into a reality! Their knowledgeable team provided me with
+                      all the information I needed to plan my ...
+                    </span>{" "}
+                  </div>
+                  <div className="text-primary font-monrope text-[14px] sm:text-[18px] xl:text-[16px] font-bold flex items-center">
+                    <span>Full Story</span>{" "}
+                    <MdOutlineKeyboardArrowRight className="text-[16px] sm:text-[28px] xl:text-[24px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-[75%]  max-h-full sm:max-h-full  h-full mx-auto   overflow-hidden flex justify-center items-center ">
+              <div className="flex flex-col md:flex-row testimonial-item w-full space-y-5 xl:space-y-0 md:space-x-[40px]  justify-center items-center px-[80px] ">
+                <div className="w-full h-[240px] sm:h-[370px] xl:w-[500px] xl:h-full ">
+                  <img
+                    src={img1}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
+                    Nazia Islam
+                  </div>
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
+                    <GoLocation /> <span>Sylhet, Bangladesh </span>
+                  </div>
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
+                    <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
+                      Aviate Abroad helped turn my dream of traveling abroad
+                      into a reality! Their knowledgeable team provided me with
+                      all the information I needed to plan my ...
+                    </span>{" "}
+                  </div>
+                  <div className="text-primary font-monrope text-[14px] sm:text-[18px] xl:text-[16px] font-bold flex items-center">
+                    <span>Full Story</span>{" "}
+                    <MdOutlineKeyboardArrowRight className="text-[16px] sm:text-[28px] xl:text-[24px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-[75%]  max-h-full sm:max-h-full  h-full mx-auto   overflow-hidden flex justify-center items-center ">
+              <div className="flex flex-col md:flex-row testimonial-item w-full space-y-5 xl:space-y-0 md:space-x-[40px]  justify-center items-center px-[80px] ">
+                <div className="w-full h-[240px] sm:h-[370px] xl:w-[500px] xl:h-full ">
+                  <img
+                    src={img1}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
+                    Nazia Islam
+                  </div>
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
+                    <GoLocation /> <span>Sylhet, Bangladesh </span>
+                  </div>
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
+                    <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
+                      Aviate Abroad helped turn my dream of traveling abroad
+                      into a reality! Their knowledgeable team provided me with
+                      all the information I needed to plan my ...
+                    </span>{" "}
+                  </div>
+                  <div className="text-primary font-monrope text-[14px] sm:text-[18px] xl:text-[16px] font-bold flex items-center">
+                    <span>Full Story</span>{" "}
+                    <MdOutlineKeyboardArrowRight className="text-[16px] sm:text-[28px] xl:text-[24px]" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-[75%]  max-h-full sm:max-h-full  h-full mx-auto   overflow-hidden flex justify-center items-center ">
+              <div className="flex flex-col md:flex-row testimonial-item w-full space-y-5 xl:space-y-0 md:space-x-[40px]  justify-center items-center px-[80px] ">
+                <div className="w-full h-[240px] sm:h-[370px] xl:w-[500px] xl:h-full ">
+                  <img
+                    src={img1}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+                <div className="w-full max-w-[600px] h-full  flex flex-col justify-center items-start">
+                  <div className="text-[18px] sm:text-[24px] md:text-[28px] xl:text-[32px] font-noto text-primary font-medium mb-2 sm:mb-[20px]">
+                    Nazia Islam
+                  </div>
+                  <div className="text-[14px] sm:text-[18px] xl:text-[18px] font-noto text-secondary font-medium mb-2 sm:mb-[20px]">
+                    <GoLocation /> <span>Sylhet, Bangladesh </span>
+                  </div>
+                  <div className="text-primary text-[16px] sm:text-[20px] space-x-[5px] xl:space-x-[8px] mb-2 sm:mb-[20px]">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                  <div className="flex justify-start mb-2 sm:mb-[20px] xl:mb-[45px]">
                     <span className="font-monrope text-[14px] sm:text-[18px]  font-normal text-left text-secondary xl:leading-[30px]">
                       Aviate Abroad helped turn my dream of traveling abroad
                       into a reality! Their knowledgeable team provided me with
